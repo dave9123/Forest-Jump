d83014edf0b726f595b20c49c68f86102f7170e6
Dave9123 <dave.turmawan@outlook.com>
2023-12-01 14:07:27 +0700

    Changed game title font size

--- scripts/home.gd
+++ scripts/home.gd
@@ -6,7 +6,7 @@ func _ready():
 	pass # Replace with function body.
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
+func _process(_delta):
 	print(Engine.get_frames_per_second())
 
 func _on_start_button_home_screen_pressed():
