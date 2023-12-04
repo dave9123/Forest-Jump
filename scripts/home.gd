5208bb1289bad45a3954cbc1eb6969c12917c6f3
Dave9123 <dave.turmawan@outlook.com>
2023-12-04 13:41:50 +0700

    Rebrand :)

--- scripts/home.gd
+++ scripts/home.gd
@@ -24,10 +24,11 @@ func _process(_delta):
 	$FPSCounter_HomeScreen.text = str(Engine.get_frames_per_second())
 
 func _on_start_button_home_screen_pressed():
-	pass
+	get_tree().change_scene_to_file("res://scenes/game.tscn")
 
 func _on_settings_button_home_screen_pressed():
 	get_tree().change_scene_to_file("res://scenes/settings.tscn")
 
 func _on_credits_button_home_screen_pressed():
 	get_tree().change_scene_to_file("res://scenes/credits.tscn")
+
