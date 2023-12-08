18e2b523dfa49cee96e16daec7a306dbfe8c1dbb
Dave9123 <dave.turmawan@outlook.com>
2023-12-08 20:16:30 +0700

    uh yes

--- scripts/controls.gd
+++ scripts/controls.gd
@@ -1,4 +1,7 @@
 extends Node2D
 
+func _process(_delta):
+	$FPSCounter_ControlsScreen.text = str(Engine.get_frames_per_second())
+
 func _on_return_button_controls_screen_pressed():
 	get_tree().change_scene_to_file("res://scenes/home.tscn")
