d2d647809b42e89e682c18c4a2dd891ee17cb0fa
Dave9123 <dave.turmawan@outlook.com>
2023-12-09 17:08:11 +0700

    ok

--- scripts/audioplayer.gd
+++ scripts/audioplayer.gd
@@ -4,3 +4,4 @@ func _ready():
 	var savefile = FileAccess.open('user://settings.dat', FileAccess.READ)
 	var _settings = savefile.get_var()
 	var volumedb = int(20*log(_settings.music_volume/100))
+	$audioplayer.volume_db = volumedb
